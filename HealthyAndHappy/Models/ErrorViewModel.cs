998bdba326246e3dbe9cd51753031ea42c84fb82
Karol Duszak <kl.duszak@gmail.com>
2020-10-29 12:46:32 +0100

    Add project files.

---
+++ HealthyAndHappy/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace HealthyAndHappy.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
