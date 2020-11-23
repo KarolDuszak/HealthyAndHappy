b61c0db35c957456673b62a530de23212e7b1290
paulinasala <sala.paulina@outlook.com>
2020-11-24 00:31:37 +0100

    Recipe&Larder interface

---
+++ HealthyAndHappy/Services/Interfaces/ILarderService.cs
@@ -0,0 +1,16 @@
+﻿using HealthyAndHappy.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HealthyAndHappy.Services.Interfaces
+{
+    public interface ILarderService
+    {
+        Larder Get(string userId);
+        List<Product> GetProductsForCategory(Product p);
+        bool ChangeAmount(Product p, int amount);
+        bool AddProduct(Product p);
+    }
+}
---
+++ HealthyAndHappy/Services/Interfaces/IRecipeService.cs
@@ -0,0 +1,17 @@
+﻿using HealthyAndHappy.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HealthyAndHappy.Services.Interfaces
+{
+    public interface IRecipeService
+    {
+        List<Recipe> GetAllRecipes();
+        List<Recipe> GetRecipesForCategory(Category catregory);
+        Recipe GetRecipeDetail(string Id);
+        bool AddRecipe();
+        
+    }
+}
