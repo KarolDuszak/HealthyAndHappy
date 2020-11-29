7cd46313c6318c095374d3e2f56985fa7f14b007
Karol Duszak <kl.duszak@gmail.com>
2020-11-29 22:05:41 +0100

    Recipe and RecipeDTO updated

--- HealthyAndHappy/Models/Recipe.cs
+++ HealthyAndHappy/Models/Recipe.cs
@@ -5,6 +5,20 @@ using System.Threading.Tasks;
 
 namespace HealthyAndHappy.Models
 {
+
+    //Categories enum contain information about assigment to cattegory
+    //for example number 5 means that meal will contain all and under500Kcal categorry
+    [Flags]
+    public enum Categories
+    {
+        None = 0b_0000_0000,            //0
+        All = 0b_0000_0001,             //1
+        Under200Kcal = 0b_0000_0010,    //2
+        Under500Kcal = 0b_0000_0100,    //4
+        More500Kcal = 0b_0000_1000,     //8
+        NoMeat = 0b_0001_0000,          //16
+        HighProtein = 0b_0010_0000,     //32
+    }
     public class Recipe
     {
         public string Id { get; set; }
@@ -14,6 +28,8 @@ namespace HealthyAndHappy.Models
         public bool IsAccepted { get; set; }
         public string IdUser { get; set; }
         public string Description { get; set; }
+        public Categories Category { get; set; }
+        public Macro Macro { get; set; }
         public List<Meat> Meats { get; set; }
         public List<Vegetable> Vegetables { get; set; }
         public List<Fruit> Fruits { get; set; }
