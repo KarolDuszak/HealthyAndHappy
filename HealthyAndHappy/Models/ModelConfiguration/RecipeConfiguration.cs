8d5d7d5624f27c09111d212f8939f11e2417968e
Karol Duszak <kl.duszak@gmail.com>
2020-12-10 12:54:33 +0100

    Recipe configuration created

--- HealthyAndHappy/Models/ModelConfiguration/RecipeConfiguration.cs
+++ HealthyAndHappy/Models/ModelConfiguration/RecipeConfiguration.cs
@@ -16,11 +16,25 @@ namespace HealthyAndHappy.Models.ModelConfiguration
             entity.Property(e=>e.PreprationTime).IsRequired();
             entity.Property(e => e.Kcal).IsRequired();
             entity.Property(e => e.Description).IsRequired();
-            entity.HasOne<ApplicationUser>(e => e.Maker);
-            entity.Property(e => e.Name).IsRequired();
-            entity.Property(e => e.Name).IsRequired();
+            entity.Property(e => e.IsAccepted).IsRequired();
+            entity.Property(e => e.Category).IsRequired();
+            entity.Property(e => e.Macro).IsRequired();
             entity.Property(e => e.imageByteArray);
-
+            entity.HasOne<ApplicationUser>(e => e.Maker);
+            entity.HasMany(e => e.Alcohols);
+            entity.HasMany(e => e.Carbohydrates);
+            entity.HasMany(e => e.Diaries);
+            entity.HasMany(e => e.Drinks);
+            entity.HasMany(e => e.DryGoods);
+            entity.HasMany(e => e.Fats);
+            entity.HasMany(e => e.FishAndSeafoods);
+            entity.HasMany(e => e.Fruits);
+            entity.HasMany(e => e.Meats);
+            entity.HasMany(e => e.Nuts);
+            entity.HasMany(e => e.Sauces);
+            entity.HasMany(e => e.Specials);
+            entity.HasMany(e => e.Spices);
+            entity.HasMany(e => e.Vegetables);
 
         }
     }
