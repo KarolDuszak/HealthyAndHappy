d867280f45fc1a8924e6d4af050947b7a65dd2a6
Karol Duszak <kl.duszak@gmail.com>
2020-12-28 18:22:35 +0100

    CookiePolicy, apDbContext and sqlServer connection string configured

---
+++ HealthyAndHappy/Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using HealthyAndHappy.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+
+namespace HealthyAndHappy.Data
+{
+    public class ApplicationDbContext: IdentityDbContext<ApplicationUser>
+    {
+
+    }
+}
